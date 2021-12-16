3169ac2e058ac883614897d50093363d30e289bc
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-12-16 01:15:03 +0100

    a small update to feestlunch.java
    not having to write out public static *insert datatype here* for every var i am creating feels much better :)

--- part 1/feestlunch.java
+++ part 1/feestlunch.java
@@ -1,10 +1,7 @@
 public class feestlunch {
-    public static int aantalCroissantjes = 17;
-    public static double prijsCroissantje = 0.39;
-    public static int aantalStokbroodjes = 2;
-    public static double prijsStokbroodje = 2.78;
-    public static int aantalBonnen = 3;
-    public static double korting = 0.5;
+    public static int aantalCroissantjes = 17, aantalStokbroodjes = 2, aantalBonnen = 3;
+    final public static double prijsCroissantje = 0.39, prijsStokbroodje = 2.78, korting = 0.5;
+    
     public static double kosten = aantalCroissantjes * prijsCroissantje + aantalStokbroodjes * prijsStokbroodje - aantalBonnen * korting;
     public static void main(String[] args){
         System.out.println(kosten);
