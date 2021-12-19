47021bd3a28fa822b9c37a1757ab65c2b2e94db2
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-12-20 00:23:10 +0100

    We got user input!
    A lot more tedious than in python. But im happy it didnt take too long to figure out. Except for that pesky warning i ended up suppressing.

--- part 1/feestlunch.java
+++ part 1/feestlunch.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+@SuppressWarnings("resource")
+
 public class feestlunch {
-    public static int aantalCroissantjes = 17, aantalStokbroodjes = 2, aantalBonnen = 3;
     final public static double prijsCroissantje = 0.39, prijsStokbroodje = 2.78, korting = 0.5;
-    
-    public static double kosten = aantalCroissantjes * prijsCroissantje + aantalStokbroodjes * prijsStokbroodje - aantalBonnen * korting;
     public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Hoeveel croissantjes wilt u?");
+        int aantalCroissantjes = userInput.nextInt();
+
+        System.out.println("Hoeveel stokbroodjes wilt u?");
+        int aantalStokbroodjes = userInput.nextInt();
+
+        System.out.println("Hoeveel kortingsbonnen heeft u?");
+        int aantalBonnen = userInput.nextInt();
+
+        double kosten = aantalCroissantjes * prijsCroissantje + aantalStokbroodjes * prijsStokbroodje - aantalBonnen * korting;
         System.out.printf(
             "De feestlunch kost je bij de bakker %.2f euro voor de %d croissantjes en de %d stokbroden als de %d kortingsbonnen nog geldig zijn!", 
             kosten, aantalCroissantjes, aantalStokbroodjes, aantalBonnen
--- part 1/speelhal.java
+++ part 1/speelhal.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+@SuppressWarnings("resource")
+
 public class speelhal {
-    public static int amountPeople = 4, minInVIPVR = 45;
     final public static double entrancePrice = 7.45, VIPVRprice = 0.37;
-    public static double totalPrice = amountPeople * entrancePrice + amountPeople * (minInVIPVR / 5 * VIPVRprice);
+
     public static void main(String[] args){
+        Scanner userAnswer = new Scanner(System.in);
+        
+        System.out.println("How many people are coming?");
+        int amountPeople = userAnswer.nextInt();
+
+        System.out.println("How many minutes will you each spend in the VIP Vr room?");
+        int minInVIPVR = userAnswer.nextInt();
+
+        double totalPrice = amountPeople * entrancePrice + amountPeople * (minInVIPVR / 5 * VIPVRprice);
         System.out.printf(
             "Dit geweldige dagje-uit met %d mensen in de Speelhal met %d minuten VR kost je maar %.2f euro",
             amountPeople, minInVIPVR, totalPrice
