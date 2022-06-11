eb37fc5f44ce2fe95f259c90b109fa751d337b68
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-06-11 12:16:53 +0200

    Created a window
    how original

---
+++ clicker/click.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class click {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Test");
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
