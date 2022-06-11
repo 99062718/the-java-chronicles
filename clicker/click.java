d5c904546c10b5a911042e7d617573f7baa24b55
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-06-11 12:23:12 +0200

    Added a button to the window
    even more original!

--- clicker/click.java
+++ clicker/click.java
@@ -3,7 +3,11 @@ import javax.swing.*;
 public class click {
     public static void main(String[] args){
         JFrame frame = new JFrame("Test");
-        frame.setSize(200, 200);
+        JButton button = new JButton("Java time");
+        button.setBounds(50, 50, 200, 20);
+        frame.add(button);
+        frame.setSize(400, 200);
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 }
